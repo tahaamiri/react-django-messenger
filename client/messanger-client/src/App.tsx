6dd53d5cd251dee24113dbdebfa5826a84311df3
Taha Amiri <amiri.taha.98@gmail.com>
2022-01-24 18:28:00 +0330

    tailwind added to project

--- client/messanger-client/src/App.tsx
+++ client/messanger-client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
+				<p className="text-red-300 ">
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
